b7a2b6155b1cdf5d79e475b4f787f572a2ae8261
justinvdm <justinvderm@gmail.com>
2025-04-16 10:53:08 +0200

    Upgrade to .61

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -1,3 +1,5 @@
+import styles from "../styles.css?url";
+
 export const Document: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
@@ -6,15 +8,16 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>RedwoodSDK</title>
-      <link href="/src/styles.css" rel="stylesheet" />
+      <link rel="preload" href="/src/client.tsx" as="script" />
+      <link href={styles} rel="stylesheet" />
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
       />
-      <script type="module" src="/src/client.tsx"></script>
     </head>
     <body>
       <div id="root">{children}</div>
+      <script src="/src/client.tsx"></script>
     </body>
   </html>
 );
--- vite.config.mts
+++ vite.config.mts
@@ -3,5 +3,7 @@ import { redwood } from "@redwoodjs/sdk/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  // context(justinvdm, 2025-04-16): Tailwind vite plugin requires SSR environment, we only have worker environment
+  environments: { ssr: {} },
   plugins: [redwood(), tailwindcss()],
 });
